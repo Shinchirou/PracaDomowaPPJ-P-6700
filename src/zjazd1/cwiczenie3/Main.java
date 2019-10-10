7afac738e7ad8b88a80dfe1c2787e380546c50a5
Shinchirou <b.ostaszewski@gmail.com>
2019-10-11 00:00:17 +0200

    Initial commit

---
+++ src/zjazd1/cwiczenie3/Main.java
@@ -0,0 +1,15 @@
+package zjazd1.cwiczenie3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TablePrinter tablePrinter = new TablePrinter();
+        tablePrinter.fillAndPrintTable(20);
+        tablePrinter.fillAndPrintTable(0);
+    }
+
+
+
+
+}
---
+++ src/zjazd1/cwiczenie3/TablePrinter.java
@@ -0,0 +1,26 @@
+package zjazd1.cwiczenie3;
+
+public class TablePrinter {
+
+    int[][] table;
+
+    public TablePrinter(){}
+
+    public void fillAndPrintTable(int n){
+
+        for(int i = 0; i <= n; i++){
+            System.out.println();
+            table = new int[i+1][i+1];
+            for (int j = 0; j <= i; j++){
+                table[i][j] = j;
+                System.out.print(table[i][j]);
+
+                if(j == i)
+                    break;
+                System.out.print(", ");
+            }
+
+        }
+
+    }
+}
